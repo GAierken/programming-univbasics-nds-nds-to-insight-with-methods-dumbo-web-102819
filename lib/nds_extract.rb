fe937a52680cb82aab384ed294e03432ecdca356
Guligena Aierken <guligena_aierken@163.com>
2019-10-24 14:54:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -19,9 +19,9 @@ def gross_for_director(director_data)
 
     index=0
     total=0
-    while index<director_data[:movies].length do
-     total+=director_data[:movies][index][:worldwide_grosses]
-     index+=1
+    while index < director_data[:movies].length do
+     total += director_data[:movies][index][:worldwide_grosses]
+     index += 1
    end
    total
 end
