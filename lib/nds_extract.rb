1ed28182e268b9f4121c33ad4b265dba97175e20
Guligena Aierken <guligena_aierken@163.com>
2019-10-24 14:41:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,7 +6,7 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-   
+
 
 
 result = {}
@@ -16,4 +16,5 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  
 end
